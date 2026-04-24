e1d08d5ddf225dd42706678e001a55e1b5d9efd2
Dwifatur12 <64035757+Dwifatur12@users.noreply.github.com>
2026-04-24 13:46:40 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  darkMode: 'class', // BARIS INI WAJIB ADA UNTUK VERCEL
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
